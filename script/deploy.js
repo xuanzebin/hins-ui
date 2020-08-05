13c7bd5d32ed6fd6ac8cf6f8f86d000c2206e614
xuanzebin <xuanzebin@126.com>
2020-08-05 23:52:49 +0800

    feat: go deploy

--- script/deploy.js
+++ script/deploy.js
@@ -4,10 +4,19 @@ const PUBLISH_TYPE = ['patch', 'minor', 'major']
 const actionIndex = PUBLISH_TYPE.indexOf(process.argv[2])
 const action = PUBLISH_TYPE[actionIndex] || 'patch'
 
+
+spawn.sync('git', ['add', '.'], {
+  stdio: 'inherit'
+})
+
+spawn.sync('git', ['commit', '-m', 'feat: go deploy'], {
+  stdio: 'inherit'
+})
+
 spawn.sync('npm', ['version', action], {
   stdio: 'inherit'
 })
 
-spawn.sync('git', ['push', '-f'], {
+spawn.sync('git', ['push', '--follow-tags', '-f'], {
   stdio: 'inherit'
 })
