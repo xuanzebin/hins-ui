4d4bb0950f0dd67c16e1e7b9af61c34f502e59b8
xuanzebin <xuanzebin@126.com>
2020-08-05 23:40:19 +0800

    feat: 完善deploy脚本

--- script/deploy.js
+++ script/deploy.js
@@ -1,6 +1,13 @@
 const spawn = require('cross-spawn')
 
-spawn.sync('npm', ['hi'], {
+const PUBLISH_TYPE = ['patch', 'minor', 'major']
+const actionIndex = PUBLISH_TYPE.indexOf(process.argv[2])
+const action = PUBLISH_TYPE[actionIndex] || 'patch'
+
+spawn.sync('npm', ['version', action], {
   stdio: 'inherit'
 })
 
+spawn.sync('git', ['push', '-f'], {
+  stdio: 'inherit'
+})
